66927cb7d296150bfc3fffe1ecc7112587f97d7b
Murat Karabulut <murat.karabulut@mobven.com>
2021-12-01 13:15:32 +0300

    API key Updated

--- app/src/main/java/com/kafein/weatherapp/util/WeatherConstants.kt
+++ app/src/main/java/com/kafein/weatherapp/util/WeatherConstants.kt
@@ -2,7 +2,7 @@ package com.kafein.weatherapp.util
 
 class WeatherConstants {
     companion object{
-        const val API_KEY = "KK00vAbvsAGQyfFYjRIP5zK5NOTO7Umt"
+        const val API_KEY = "xMsH53eqkY40Pn2zkD9tAgTNGpZnY1h2"
         const val LANGUAGE = "tr-TR"
         const val SPESIFIC_COUNTRY = "TR"
         const val LAST_SEARCH_LIST = "last_search_list"
